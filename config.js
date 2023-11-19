11703f02bc293d9cb72f754f8cacc761c5b67424
Nguyen Quang Thuan <thuan13112003@gmail.com>
2023-11-20 05:26:49 +0700

    created by NQT

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = "Thu Hà";
+const giftUrl = "https://www.facebook.com/nguyenthuan2k3";
+const eventName = "Chúc Mừng 20-11";
+const titleCard = "Tặng cô giáo Thu Hà";
+const contentCard =
+  "Nhân Ngày Nhà giáo Việt Nam 20-11, xin chúc cho cậu luôn luôn xinh đẹp, hạnh phúc và thật thành công trong sự nghiệp trồng người 💔";
+
+const giftImage = "images/ha.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
